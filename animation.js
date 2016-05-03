d2022641b43c6eb31e39b204dc98b427fcea8153
crystal-b <git@github.com:crystal-b>
2016-05-03 02:44:23 -0400

    testing gh-pages

---
+++ animation.js
@@ -0,0 +1,120 @@
+$(document).ready(function() {
+	console.log("start story");
+
+});
+
+var time = x;
+
+function one() {
+	console.log(".");
+	timer2();
+}
+function two() {
+	console.log(" .");
+	timer3();
+}
+function three() {
+	console.log("  .");
+	timer4();
+}
+function four() {
+	console.log("   .");
+	timer5();
+}
+function five() {
+	console.log("    .");
+	timer6();
+}
+function six() {
+	console.log("    .");
+	timer7();
+}
+function seven() {
+	console.log("     .");
+	timer8();
+}
+function eight() {
+	console.log("      .");
+	timer9();
+}
+function nine() {
+	console.log("       .");
+	timer10();
+}
+function ten() {
+	console.log("        .");
+}
+
+
+function timer1() {
+	setTimeout(clear1, time);	
+}
+function timer2() {
+	setTimeout(clear2, time);	
+}
+function timer3() {
+	setTimeout(clear3, time);	
+}
+function timer4() {
+	setTimeout(clear4, time);	
+}
+function timer5() {
+	setTimeout(clear5, time);	
+}
+function timer6() {
+	setTimeout(clear6, time);	
+}
+function timer7() {
+	setTimeout(clear7, time);	
+}
+function timer8() {
+	setTimeout(clear8, time);	
+}
+function timer9() {
+	setTimeout(clear9, time);	
+}
+function timer10() {
+	setTimeout(clear10, time);	
+}
+
+
+function clear1() {
+	console.clear();
+	one();
+}
+function clear2() {
+	console.clear();
+	two();
+}
+function clear3() {
+	console.clear();
+	three();
+}
+function clear4() {
+	console.clear();
+	four();
+}
+function clear5() {
+	console.clear();
+	five();
+}
+function clear6() {
+	console.clear();
+	six();
+}
+function clear7() {
+	console.clear();
+	seven();
+}
+function clear8() {
+	console.clear();
+	eight();
+}
+function clear9() {
+	console.clear();
+	nine();
+}
+function clear10() {
+	console.clear();
+	ten();
+}
